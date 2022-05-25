82cee87afd97745eaee62ef90c093d51d6dd3794
FelipeMg01 <102561544+FelipeMg01@users.noreply.github.com>
2022-05-25 19:12:37 -0300

    Add files via upload

---
+++ aula01/aula01/www/js/add.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy'
+    });
+    $('.fixed-action-btn').floatingActionButton();
+
+ $('#add-btn').click(function(){
+     let tarefa = $('#tarefa')
+
+ });
+});
